aaca522296517664b432c2cfa5eac66d3af1eac6
Zhou Zach <wander669@gmail.com>
2017-03-27 00:01:52 +0800

    replace command2protocal.sh with awk

--- command2protocol.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-while read CMD; do
-  # each command begins with *{number arguments in command}\r\n
-  XS=($CMD); printf "*${#XS[@]}\r\n"
-  # for each argument, we append ${length}\r\n{argument}\r\n
-  for X in $CMD; do printf "\$${#X}\r\n$X\r\n"; done
-done
--- impala2redis.sh
+++ impala2redis.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-impala-shell -q 'select clientid from data_db_1487059346591.log;' | awk '{print "pfadd all_uv " $2}' | sed '1,3d' | sed '$d' | sh command2protocol.sh | redis-cli --pipe
+impala-shell -q 'select clientid from data_db_1487059346591.log;' | awk '{print "pfadd all_uv " $2}' | sed '1,3d' | sed '$d' | awk '{print "\*" NF; print "\$" length($1); print $1; print "\$" length($2); print $2; print "\$" length($3); print $3;}' | redis-cli --pipe
